7f071a5bb5defa8a7972569d0f2401bd034e34fa
Dongle <programcake@gmail.com>
2022-12-18 22:14:57 +0700

    secure URL

--- src/lib/Metadata.svelte
+++ src/lib/Metadata.svelte
@@ -9,6 +9,9 @@
 <title>{title}</title>
 <meta content={title} name="og:title">
 <meta content={title} name="twitter:title">
-<meta content="https://{$page.url.host}{heroImage}" name="og:image">
+
+{#if heroImage}
+<meta content="https://{$page.url.host}{heroImage}" name="og:image:secure_url">
 <meta content="https://{$page.url.host}{heroImage}" name="twitter:image:src">
 <meta name="twitter:card" content="summary_large_image">
+{/if}
