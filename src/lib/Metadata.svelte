fd148affda3862b46a089f35256a800444a06361
Dongle <programcake@gmail.com>
2022-12-18 22:06:39 +0700

    Does this work?

--- src/lib/Metadata.svelte
+++ src/lib/Metadata.svelte
@@ -11,7 +11,7 @@
 <meta content={title} name="twitter:title">
 
 {#if heroImage}
-<meta content={heroImage} name="og:image">
+<meta content="https://{$page.url.host}{heroImage}" name="og:image">
 <meta content="https://{$page.url.host}{heroImage}" name="twitter:image">
 <meta name="twitter:card" content="summary_large_image">
 {/if}
