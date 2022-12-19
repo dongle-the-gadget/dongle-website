8c45dc06cd52ef45d60dd466f5419df54ca7181f
Dongle <programcake@gmail.com>
2022-12-19 16:08:50 +0700

    Svelte broke the app...

--- src/lib/Metadata.svelte
+++ src/lib/Metadata.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { page } from "$app/stores";
-
 	export let title = "Dongle";
 
 	export let heroImage = "";
@@ -9,8 +7,8 @@
 <title>{title}</title>
 <meta content={title} name="og:title">
 <meta content={title} name="twitter:title">
-<meta content="https://{$page.url.host}{heroImage}" name="og:image">
-<meta content="https://{$page.url.host}{heroImage}" name="og:image:secure_url">
-<meta content="https://{$page.url.host}{heroImage}" name="twitter:image">
-<meta content="https://{$page.url.host}{heroImage}" name="twitter:image:src">
+<meta content="https://www.roundadorner.tk{heroImage}" name="og:image">
+<meta content="https://www.roundadorner.tk{heroImage}" name="og:image:secure_url">
+<meta content="https://www.roundadorner.tk{heroImage}" name="twitter:image">
+<meta content="https://www.roundadorner.tk{heroImage}" name="twitter:image:src">
 <meta name="twitter:card" content="summary_large_image">
