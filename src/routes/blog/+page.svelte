c44a45818e773e17792cd6baf5ff2d112afe03f6
Dongle <29563098+dongle-the-gadget@users.noreply.github.com>
2023-12-08 23:32:08 +0700

    Attempt fix #2.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -11,8 +11,6 @@
 	let scrollY: number;
     
     export const widgetsSuffix = $page.url.searchParams.has('widgets') ? "_blank": null;
-	export const prerender = false;
-	export const ssr = true;
 </script>
 
 <Metadata title="Dongle • Blog" />
